ab314ab28356535245a987da92e0fe7ff347ebab
Devin Dang <devin.dang2015@gmail.com>
2019-07-02 22:55:13 -0400

    Finished setting up WMATA API
    Able to use API within the app

--- train-app/src/App.js
+++ train-app/src/App.js
@@ -2,6 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// uses the WMATA Train Positions API
+class TrainPositions extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      TrainPositions: {}
+    };
+  }
+
+  //fetch the WMATA Train Positions API
+  componentDidMount()
+  {
+    // API URL
+    var url = 'https://api.wmata.com/TrainPositions/TrainPositions?contentType=json';
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        // in a real app, need to hide api key
+        'api_key': '8f953318a5d847e2b8f4fcca7c5d1b4c'
+      }
+    })
+    .then(res => res.json())
+    .then(
+      // set state if api is accessed
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          TrainPositions: result.TrainPositions
+        });
+      },
+      // set error if api cannot be accessed
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +64,13 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          { TrainPositions.state.TrainPositions.map(TrainPositions =>
+            <li key={TrainPositions.TrainId}>
+              {TrainPositions.TrainId}
+            </li>
+          )}
+        </ul>
       </header>
     </div>
   );
